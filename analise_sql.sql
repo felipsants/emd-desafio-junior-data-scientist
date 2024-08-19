595c7d1311d7f606cb640db8354391f153ba83e6
felipsants <165330370+felipsants@users.noreply.github.com>
2024-08-19 18:40:23 -0300

    Update questão 3 e questão 5 do perguntas_sql concluida

--- analise_sql.sql
+++ analise_sql.sql
@@ -30,8 +30,8 @@ id de identificação, dessa forma eu desconsiderei o 3º colocado e fui para o 
 ocorrências.
     1º : Campo Grande, ID 144 - Chamados : 113
     2º : Tijuca, ID 33 - Chamados : 89
-    3º : NULL - Chamados : 73
-    4º : Barra da Tijuca, ID 128 - Chamados : 59
+    3º : Barra da Tijuca, ID 128 - Chamados : 59
+    3º : NULL - Chamados : 73 ( Provavelmente, são os chamados que não tem ligação com um lugar fisico.)
 */
 
 -- Questão 4: Qual o nome da subprefeitura com mais chamados abertos nesse dia?
@@ -50,3 +50,13 @@ Nessa questão eu decidi usar alias pois estou trabalhando com duas fontes de da
 que tem a mesma coluna id_bairro, assim facilita a leitura.
 */
 
+-- Questão 5: Existe algum chamado aberto nesse dia que não foi associado a um bairro ou subprefeitura na tabela de bairros?Se sim, por que isso acontece?
+SELECT *
+FROM `datario.adm_central_atendimento_1746.chamado`
+WHERE DATE(data_inicio) = '2023-04-01' 
+  AND id_bairro IS NULL
+LIMIT 50
+
+/* Resposta: 
+    Sim, existe. Isso ocorre nas chamadas que são relacionadas ao transporte público, no caso
+    ônibus, que não tem ligação com um lugar fisico, que no nosso caso de interesse seria um bairro.
